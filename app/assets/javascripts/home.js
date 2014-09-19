4279abdf6166ce653398135e3d380f1c61a89ee7
Austin Sprawls <austinsprawls@gmail.com>
2014-09-19 13:07:05 -0500

    basically completed home/landing page view

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,4 +1,12 @@
-$(window).scroll(function(){
-  $('.navbar-index').removeClass('nav-index-top');
-  $('.navbar-index').addClass('nav-index-scroll');
-});
+
+window.onscroll = scroll;
+
+function scroll(){
+  if(window.pageYOffset > 1){
+    $('.navbar-index').removeClass('nav-index-top');
+    $('.navbar-index').addClass('nav-index-scroll');
+  }else{
+    $('.navbar-index').addClass('nav-index-top');
+    $('.navbar-index').removeClass('nav-index-scroll');
+  }
+}
