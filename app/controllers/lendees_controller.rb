72efe060f1f1d15f85c47668966c4f7b3e9cf252
Austin Sprawls <austinsprawls@gmail.com>
2014-09-20 19:19:03 -0500

    refactored lendee show view and lendee controller to take db calls out of view

--- app/controllers/lendees_controller.rb
+++ app/controllers/lendees_controller.rb
@@ -2,8 +2,11 @@ class LendeesController < ApplicationController
   before_action :authenticate_lendee!
 
   def show
-    @lendee = Lendee.find(current_lendee.id)
-    @investments = Investment.where(lender_id: current_lendee.id)
+    @lendee = current_lendee
+    @loan = current_lendee.loan
+    @investments = Investment.where(loan_id: @loan.id).limit(7)
+    @amount_funded = @loan.amount_funded
+    @amount_requested = @loan.amount
   end
 
   def edit
