ce93d38192659da775629673dbd9d7071223f18b
Austin Sprawls <austinsprawls@gmail.com>
2014-09-22 16:33:55 -0500

    added investment functionality and progress bar to loans index view

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -5,8 +5,16 @@ class InvestmentsController < ApplicationController
       flash[:alert] = "You must be registered as an investor to invest"
     elsif !current_lender
       flash[:alert] = "You must be signed in to invest"
+    else
+      Investment.create(lender_id: current_lender.id, loan_id: strong_params[:loan_id], amount: strong_params[:amount].to_f)
+      Loan.find(strong_params[:loan_id]).increment!(:amount_funded, strong_params[:amount].to_f)
+      flash[:success] = "You successfully invested $#{strong_params[:amount]}"
     end
     redirect_to browse_loans_path
+  end
+
+  def strong_params
+    params.require(:investment).permit(:amount, :loan_id)
   end
 
 end
