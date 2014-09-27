81842818d5656a61095d97da02affc6845093d66
Austin Sprawls <austinsprawls@gmail.com>
2014-09-27 14:07:33 -0500

    added ability for lendee to edit/update their payments

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -12,4 +12,27 @@ class PaymentsController < ApplicationController
     @payment = Payment.find(params[:id])
   end
 
+  def edit
+    @payment = Payment.find(params[:id])
+    render layout: 'lendees'
+  end
+
+  def update
+    payment = Payment.find(params[:id])
+    amount_paid = payment_params[:amount_paid].to_f
+    if amount_paid <= payment.amount && amount_paid > 0
+      payment.update_attributes(amount_paid: amount_paid)
+      payment.update_attributes(paid?: true) if payment.amount_paid == payment.amount
+      flash[:success] = "You successfully paid $#{amount_paid}"
+      redirect_to profile_path
+    else
+      flash[:alert] = "You must enter a value greater than zero and less than $#{payment.amount}"
+      redirect_to edit_lendee_payment_path(params[:lendee_id], params[:id])
+    end
+  end
+
+  def payment_params
+    params.require(:payment).permit(:amount_paid)
+  end
+
 end
--- lib/transaction_scripts/create_payment.rb
+++ lib/transaction_scripts/create_payment.rb
@@ -5,8 +5,8 @@ class CreatePayment
     investments_to_pay = Investment.where(loan_id: loan.id)
 
     investments_to_pay.each do |investment|
-      payment_amount = (investment.amount/loan.term)+(investment.amount/loan.term*loan.rate/100)
-      Payment.create(lender_id: investment.lender_id, lendee_id: lendee.id, amount: payment_amount )
+      payment_amount = ((investment.amount/loan.term)+(investment.amount/loan.term*loan.rate/100)).round(2)
+      Payment.create(lender_id: investment.lender_id, lendee_id: lendee.id, amount: payment_amount, due_date: Date.today+1.month )
     end
 
   end
