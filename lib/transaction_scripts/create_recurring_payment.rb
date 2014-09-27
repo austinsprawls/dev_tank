63213a5349724d82351a50f621db456c2c704c47
Austin Sprawls <austinsprawls@gmail.com>
2014-09-27 15:18:52 -0500

    cleaned up recurring payment transaction script/removed debugging puts statements

--- lib/transaction_scripts/create_recurring_payment.rb
+++ lib/transaction_scripts/create_recurring_payment.rb
@@ -1,6 +1,5 @@
 class CreateRecurringPayment
   def self.run(params)
-    puts "now entered CreateRecurringPayment txs"
     payment = params[:payment]
     lender_id = payment.lender_id
     lendee_id = payment.lendee_id
@@ -10,11 +9,7 @@ class CreateRecurringPayment
     all_payments = Payment.where(lendee_id: lendee_id, paid?: true)
     total_paid = all_payments.map{|payment| payment.amount_paid}.inject(:+)
 
-    puts "total paid: #{total_paid}"
-    puts "loan_amount_requested: #{loan_amount_requested}"
-
     unless total_paid >= loan_amount_requested
-      puts "entered the conditional"
       Payment.create(lender_id: lender_id, lendee_id: lendee_id, amount: payment_amount, due_date: payment.due_date+1.month)
     end
   end
