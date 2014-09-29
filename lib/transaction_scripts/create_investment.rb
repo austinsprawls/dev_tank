951e79a0148ea5108ba239d92416e51a5f68cb52
Austin Sprawls <austinsprawls@gmail.com>
2014-09-29 11:34:26 -0500

    added error handling to investment controller so lender can't invest more than available funds

--- lib/transaction_scripts/create_investment.rb
+++ lib/transaction_scripts/create_investment.rb
@@ -5,7 +5,7 @@ class CreateInvestment
     flash = strong_params[:flash]
     current_lendee = strong_params[:current_lendee]
     current_lender = strong_params[:current_lender]
-    amount = strong_params[:amount]
+    amount = strong_params[:amount].to_f
     loan_id = strong_params[:loan_id]
     loan = Loan.find(loan_id)
 
@@ -15,16 +15,18 @@ class CreateInvestment
       flash[:alert] = "You must be signed in to invest"
     elsif amount.blank?
       flash[:alert] = "You must enter an amount to invest"
-    elsif amount.to_f < 25
+    elsif amount < 25
       flash[:alert] = "Investment must be at least $25"
-    elsif amount.to_f > loan.amount_remaining
+    elsif amount > current_lender.funds
+      flash[:alert] = "You don't have enough funds to invest $#{amount}"
+    elsif amount > loan.amount_remaining
       flash[:alert] = "You can't invest more than the amount remaining on the loan"
     else
-      expected_return = (loan.rate/100) * amount.to_f
-      Investment.create(lender_id: current_lender.id, loan_id: loan_id, amount: amount.to_f, expected_return: expected_return)
-      current_lender.increment!(:total_invested, amount.to_f)
-      loan.increment!(:amount_funded, amount.to_f)
-      loan.decrement!(:amount_remaining, amount.to_f)
+      expected_return = (loan.rate/100) * amount
+      Investment.create(lender_id: current_lender.id, loan_id: loan_id, amount: amount, expected_return: expected_return)
+      current_lender.increment!(:total_invested, amount)
+      loan.increment!(:amount_funded, amount)
+      loan.decrement!(:amount_remaining, amount)
       loan.update_attributes(funded?: true) if loan.amount_funded >= loan.amount_requested
       if loan.funded?
         CreatePayments.run(loan: loan)
