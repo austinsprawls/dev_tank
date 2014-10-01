30a3b27286ed6af35bcd66841b0a3d19b13e294b
Austin Sprawls <austinsprawls@gmail.com>
2014-10-01 12:22:21 -0500

    wrote validation tests for lender class

--- spec/models/lender_spec.rb
+++ spec/models/lender_spec.rb
@@ -11,4 +11,41 @@ describe Lender do
   it { should respond_to(:email)}
   it { should respond_to(:password)}
   it { should respond_to(:state_of_residence)}
+
+  it { should be_valid}
+
+  describe "when email is not present" do
+    before { @lender.email = " "}
+    it { should_not be_valid}
+  end
+
+  describe "when password is not present" do
+     before { @lender.password = " "}
+     it { should_not be_valid}
+  end
+
+  describe "when state of residence is not present" do
+    before { @lender.state_of_residence = " "}
+    it { should_not be_valid}
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @lender.email = valid_address
+        expect(@lender).to be_valid
+      end
+    end
+  end
+
+  describe "when email address is already taken" do
+    before do
+      lender_with_same_email = @lender.dup
+      lender_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
+
 end
